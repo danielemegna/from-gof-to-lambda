8d69501df582a6e409e5c2029e82d62bec01199b
Daniele Megna <daniele.megna@xpeppers.com>
2016-10-06 20:50:01 +0200

    Just improved ChainOfResp object version

--- src/main/java/org/mfusco/fromgoftolambda/talk/chainofresponsibility/ChainOfRespGoF.java
+++ src/main/java/org/mfusco/fromgoftolambda/talk/chainofresponsibility/ChainOfRespGoF.java
@@ -3,83 +3,92 @@ package org.mfusco.fromgoftolambda.talk.chainofresponsibility;
 public class ChainOfRespGoF {
 
     public interface FileParser {
-        String parse( File file );
-        void setNextParser( FileParser next );
+        String parse(File file);
+
+        void setNextParser(FileParser next);
     }
 
     public abstract static class AbstractFileParser implements FileParser {
         protected FileParser next;
 
         @Override
-        public void setNextParser( FileParser next ) {
+        public void setNextParser(FileParser next) {
             this.next = next;
         }
     }
 
     public static class TextFileParser extends AbstractFileParser {
         @Override
-        public String parse( File file ) {
-            if ( file.getType() == File.Type.TEXT ) {
+        public String parse(File file) {
+            if (file.getType() == File.Type.TEXT) {
                 return "Text file: " + file.getContent();
             } else if (next != null) {
-                return next.parse( file );
+                return next.parse(file);
             } else {
-                throw new RuntimeException( "Unknown file: " + file );
+                throw new RuntimeException("Unknown file: " + file);
             }
         }
     }
 
     public static class PresentationFileParser extends AbstractFileParser {
         @Override
-        public String parse( File file ) {
-            if ( file.getType() == File.Type.PRESENTATION ) {
+        public String parse(File file) {
+            if (file.getType() == File.Type.PRESENTATION) {
                 return "Presentation file: " + file.getContent();
             } else if (next != null) {
-                return next.parse( file );
+                return next.parse(file);
             } else {
-                throw new RuntimeException( "Unknown file: " + file );
+                throw new RuntimeException("Unknown file: " + file);
             }
         }
     }
 
     public static class AudioFileParser extends AbstractFileParser {
         @Override
-        public String parse( File file ) {
-            if ( file.getType() == File.Type.AUDIO ) {
+        public String parse(File file) {
+            if (file.getType() == File.Type.AUDIO) {
                 return "Audio file: " + file.getContent();
             } else if (next != null) {
-                return next.parse( file );
+                return next.parse(file);
             } else {
-                throw new RuntimeException( "Unknown file: " + file );
+                throw new RuntimeException("Unknown file: " + file);
             }
         }
     }
 
     public static class VideoFileParser extends AbstractFileParser {
         @Override
-        public String parse( File file ) {
-            if ( file.getType() == File.Type.VIDEO ) {
+        public String parse(File file) {
+            if (file.getType() == File.Type.VIDEO) {
                 return "Video file: " + file.getContent();
             } else if (next != null) {
-                return next.parse( file );
+                return next.parse(file);
             } else {
-                throw new RuntimeException( "Unknown file: " + file );
+                throw new RuntimeException("Unknown file: " + file);
             }
         }
     }
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         FileParser textParser = new TextFileParser();
         FileParser presentationParser = new PresentationFileParser();
         FileParser audioParser = new AudioFileParser();
         FileParser videoParser = new VideoFileParser();
 
-        textParser.setNextParser( presentationParser );
-        presentationParser.setNextParser( audioParser );
-        audioParser.setNextParser( videoParser );
+        textParser.setNextParser(presentationParser);
+        presentationParser.setNextParser(audioParser);
+        audioParser.setNextParser(videoParser);
 
-        File file = new File( File.Type.AUDIO, "Dream Theater  - The Astonishing" );
+        File file = new File(File.Type.AUDIO, "Dream Theater  - The Astonishing");
+        System.out.println(textParser.parse(file));
 
-        System.out.println( textParser.parse( file ) );
+        file = new File(File.Type.VIDEO, "A cool video file!");
+        System.out.println(textParser.parse(file));
+
+        file = new File(File.Type.PRESENTATION, "A cool presentation file!");
+        System.out.println(textParser.parse(file));
+
+        file = new File(File.Type.TEXT, "A cool text file!");
+        System.out.println(textParser.parse(file));
     }
 }
