3bd543dc58f7de3c224de8e019e942b8b98ffc5a
Daniele Megna <daniele.megna@xpeppers.com>
2016-10-06 21:08:18 +0200

    First functional solutions with Optional and orElse

--- src/main/java/org/mfusco/fromgoftolambda/talk/chainofresponsibility/ChainOfRespLambda.java
+++ src/main/java/org/mfusco/fromgoftolambda/talk/chainofresponsibility/ChainOfRespLambda.java
@@ -1,9 +1,61 @@
 package org.mfusco.fromgoftolambda.talk.chainofresponsibility;
 
+import java.util.Optional;
+
 public class ChainOfRespLambda {
 
-    public static void main( String[] args ) {
-        File file = new File( File.Type.AUDIO, "Dream Theater  - The Astonishing" );
+    public static Optional<String> parseText(File file) {
+        if (file.getType() == File.Type.TEXT) {
+            return Optional.of("Text file: " + file.getContent());
+        } else {
+            return Optional.empty();
+        }
+    }
 
+    public static Optional<String> parsePresentation(File file) {
+        if (file.getType() == File.Type.PRESENTATION) {
+            return Optional.of("Presentation file: " + file.getContent());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<String> parseAudio(File file) {
+        if (file.getType() == File.Type.AUDIO) {
+            return Optional.of("Audio file: " + file.getContent());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<String> parseVideo(File file) {
+        if (file.getType() == File.Type.VIDEO) {
+            return Optional.of("Video file: " + file.getContent());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File(File.Type.AUDIO, "Dream Theater  - The Astonishing");
+        work(file);
+
+        file = new File(File.Type.VIDEO, "A cool video file!");
+        work(file);
+
+        file = new File(File.Type.PRESENTATION, "A cool presentation file!");
+        work(file);
+
+        file = new File(File.Type.TEXT, "A cool text file!");
+        work(file);
+    }
+
+    private static void work(File file) {
+        System.out.println(parseText(file).orElse(
+                parsePresentation(file).orElse(
+                        parseAudio(file).orElse(
+                                parseVideo(file).orElse(
+                                        "Unknown file"
+                                )))));
     }
 }
