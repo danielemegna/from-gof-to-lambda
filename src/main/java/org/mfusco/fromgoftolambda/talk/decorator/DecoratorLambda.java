dcb0970d68c1ff5e7b505fe96222ea8e521ea2fb
Daniele Megna <daniele.megna@xpeppers.com>
2016-10-06 20:14:21 +0200

    Done Decorator pattern

--- src/main/java/org/mfusco/fromgoftolambda/talk/decorator/DecoratorLambda.java
+++ src/main/java/org/mfusco/fromgoftolambda/talk/decorator/DecoratorLambda.java
@@ -1,8 +1,48 @@
 package org.mfusco.fromgoftolambda.talk.decorator;
 
+import java.util.function.DoubleUnaryOperator;
+
 public class DecoratorLambda {
 
-    public static void main( String[] args ) {
+    public static class DefaultSalaryCalculator implements DoubleUnaryOperator {
 
+        @Override
+        public double applyAsDouble(double grossAnnual) {
+            return grossAnnual / 12;
+        }
+    }
+
+    public static class GeneralTaxDecorator implements DoubleUnaryOperator {
+
+        @Override
+        public double applyAsDouble(double salary) {
+            return Taxes.generalTax(salary);
+        }
+    }
+
+    public static class RegionalTaxDecorator implements DoubleUnaryOperator {
+
+        @Override
+        public double applyAsDouble(double salary) {
+            return Taxes.regionalTax(salary);
+        }
+    }
+
+    public static class HealthInsuranceDecorator implements DoubleUnaryOperator {
+
+        @Override
+        public double applyAsDouble(double salary) {
+            return Taxes.healthInsurance(salary);
+        }
+    }
+
+    public static void main(String[] args) {
+        double result = new DefaultSalaryCalculator()
+                .andThen(new GeneralTaxDecorator())
+                .andThen(new RegionalTaxDecorator())
+                .andThen(new HealthInsuranceDecorator())
+                .applyAsDouble(30000.00);
+
+        System.out.println(result);
     }
 }
